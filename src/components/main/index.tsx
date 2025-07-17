68b855c58be966c460663079dfbcef2b2e270b6d
IvanChyrych <i9901432@gmail.com>
2025-07-17 14:37:28 +0300

    add main component

---
+++ src/components/main/index.tsx
@@ -0,0 +1,13 @@
+import type {ReactNode} from "react";
+
+interface MainProps {
+    children:ReactNode;
+}
+
+export const Main:React.FC<MainProps> = ({children}) => {
+    return (
+        <div className="d-flex">
+            {children}
+        </div>
+    );
+};
