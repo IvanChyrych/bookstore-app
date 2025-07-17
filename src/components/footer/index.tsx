c16720d6295cba71d1d34a9b2bf36fb286c0deb4
IvanChyrych <i9901432@gmail.com>
2025-07-17 14:42:18 +0300

    add footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Footer:React.FC=()=>{
+    return(
+        <div className="footer">
+            <p>© 2025 Bookstore. All rights reserved.</p>
+        </div>
+    )
+}
+
