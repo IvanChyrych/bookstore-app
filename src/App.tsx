b5b37ec4941f31c0ae14b2af8862abd1b6ad090b
IvanChyrych <i9901432@gmail.com>
2025-07-17 18:05:21 +0300

    add card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './styles.scss'
 import { Layout } from './components/layout'
 import { Title } from './components/title'
+import { Card } from './components/card'
+
+
 
 export function App() {
 
   return (
-    <Layout><Title>New releases</Title></Layout>
+    <Layout>
+      <Title>New releases</Title>
+      <Card title='book title' text='dsfvsd' price='213' rating='32' image='.\src\assets\book-placeholder.png'/>
+    </Layout>
   )
 }
 
