268f8f64ed92b5184fcbbeadec13117247246582
IvanChyrych <i9901432@gmail.com>
2025-07-24 15:06:02 +0300

    add homepage link to header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -8,7 +8,9 @@ export const Header: React.FC<HeaderProps> = () => {
     return (
         <nav className="navbar bg-body-tertiary">
             <div className="container-fluid">
+                <NavLink className='nav-link' to='/'>
                 <a className="navbar-brand">Navbar</a>
+                </NavLink>
                 <div className="navbar-nav flex-row">
                     <NavLink className='nav-link px-2' to='/books/favourites'>Favourites</NavLink>
                     <a className='nav-link px-2' href="">lore2</a>
